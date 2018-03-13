582b9a494ef6558584ff18bb10017962dd82f422
Marek Aleszczyk <marek@e-kreatywnie.pl>
2018-03-13 11:53:04 +0100

    -

--- class-woocommerce-custom-my-account-endpoint.php
+++ class-woocommerce-custom-my-account-endpoint.php
@@ -4,17 +4,18 @@ require_once('trait-wp-auto-hooks.php');
 
 // Add an endpoint to the My Account page
 class Woocommerce_Custom_My_Account_Endpoint {
-	use wpAutoHooks;
-	
-	private $name = '';
-	private $title = '';
-	
-	public function __construct( $name, $title ) {
-		$this->name = trim($name);
+    use wpAutoHooks;
+
+    protected $name  = '';
+    protected $title = '';
+    private $show_pagination = TRUE;
+
+    public function __construct( $name, $title ) {
+		$this->name  = trim($name);
 		$this->title = trim($title);
 
-		// Install TODO
-		//register_activation_hook( plugin_basename( __FILE__ ), [ $this, 'flush_rewrite_rules'] );
+		// Install
+		register_activation_hook( $GLOBALS['plugin'], [ $this, 'flush_rewrite_rules'] );
 
 		add_action( 'woocommerce_account_' . $this->name . '_endpoint', [ $this, 'endpoint_content'] );
 		
@@ -27,7 +28,9 @@ class Woocommerce_Custom_My_Account_Endpoint {
 		add_rewrite_endpoint( $this->name, EP_PAGES );
 
 		// Flush Permalinks
-		flush_rewrite_rules();
+		if ( !has_action( 'shutdown', 'flush_rewrite_rules' ) ) {
+                    add_action( 'shutdown', 'flush_rewrite_rules' );
+                }
 	}
 	
 	public function init_wpaction() {
@@ -83,24 +86,19 @@ class Woocommerce_Custom_My_Account_Endpoint {
 		$current_page = empty( $_current_page ) ? 1 : absint( $_current_page );
 		$name = $this->name;
 
-		if ( is_callable( [ $this, $name ] ) ) { $this->$name( $current_page ); }
-		else {
-			$this->no_content( $current_page );
-		}
+		$this->output( $current_page );
+		$this->pagination( $current_page );
 	}
 	
 	public function woocommerce_after_my_account_wpaction() {
 		self::hook_check(__FUNCTION__);
 		
 		if ( version_compare( WC()->version, '2.6', '<' ) ) {
-			if ( is_callable( [ $this, $name ] ) ) { $this->$name(); }
-			else {
-				$this->no_content();
-			}
+			$this->output();
 		}
 	}
 	
-	protected function no_content( $current_page=NULL ) { ?>
+	protected function output( $current_page=NULL ) { ?>
 		
 		<?php if ( !is_null( $current_page ) ) : ?>
 		<p>
@@ -108,14 +106,40 @@ class Woocommerce_Custom_My_Account_Endpoint {
 		</p>
 		<?php endif; ?>
 		<p>
-			<?php esc_html_e( 'No method with content defined. Please define '); ?>
-			<code><?php  esc_html_e( static::class . '::' . $this->name ); ?></code>
+			<?php esc_html_e( 'No method with content defined. Please define: '); ?>
+		<p><code><?php  esc_html_e( static::class . '::output( $current_page )' ); ?></code></p>
 			<?php 
 			esc_html_e( ' method' ); 
 			esc_html_e( (self::class === static::class ) ? ' in a child class.' : '.' ) ; 
 			?>
 		</p>
 	<?php }
+	
+	protected function pagination( $current_page ) { ?>
+                <?php if ( !$this->show_pagination() ) : return; endif;?>
+		<nav class="woocommerce-myaccount-endpoint-pagination">
+			<a class="prev<?php echo $current_page == 1 ? ' disabled' : '' ?>"
+			   <?php if ( $current_page > 1 ) : ?>
+			   href="<?php echo esc_url($this->url()) . ( $current_page-1 ); ?>"
+			   <?php endif; ?>>
+				Previous
+			</a>
+			<a class="next" 
+			   href="<?php echo esc_url($this->url()) . ( $current_page+1 ); ?>">
+				Next
+			</a>
+		</nav>
+	<?php }
+        
+        protected function show_pagination( $show=NULL ) {
+            if (is_null($show)) { return $this->show_pagination; }
+            $this->show_pagination = (bool)$show;
+            return $this->show_pagination;
+        }
+	
+	public function url() {
+		return wc_get_endpoint_url( $this->name, '', wc_get_page_permalink( 'myaccount' ) );
+	}
 }
 
 
